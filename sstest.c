042ed4bb0ef42955c3aa8999b708329845111256
Anatoly <champi2061@mail.ru>
2019-07-12 14:00:32 +0300

    Add files via upload

--- sstest.c
+++ sstest.c
@@ -1,33 +1,55 @@
 #include "libschsat.h"
 
+#define mlrd 1000000000
 uint64_t dataX, dataY, dataZ;
 uint32_t vecX, vecY, vecZ;
 uint8_t tempX, tempY, tempZ;
-bool actualX, actualY, actualZ;
-uint32_t vecXA[72], vecYA[72], vecZA[72];
+uint32_t vecXA[72][2], vecYA[72][2], vecZA[72][2];
+float XYZ[3];
+float angle = 0.0;
 void control()
 {
-	for (i = 0; i < 360; i += 5)
+	for (int i = 0; i < 72; i++)
 	{
+		angle += 5;
+		XYZ[0] = angle;
+		XYZ[1] = 0;
+		XYZ[2] = 0;
+		send_unican_message(0x5, 0xF010, (uint8_t*)(XYZ), 96);
 		recieve_unican_message(0x10, 0xB21, (uint8_t*)(&dataX), 48, 1000);
 		vecX = dataX >> 16; vecY = dataY >> 16; vecZ = dataZ >> 16;
 		dataX = dataX << 32; dataY = dataY << 32; dataZ = dataZ << 32;
 		tempX = dataX >> 8; tempY = dataY >> 8; tempZ = dataZ >> 8;
 		dataX = dataX << 8; dataY = dataY << 8; dataZ = dataZ << 8;
-		actualX = dataX >> 7; actualY = dataY >> 7; actualZ = dataZ >> 7;
-		printf("Iterator: %d / 360. \n Vector X: %d ; Vector Y: %d ; Vector Z: %d ;\nTemp X: %d ; Temp Y: %d ; Temp Z: %d ;\n\n *************************************** \n\n", i, vecX, vecY, vecZ, tempX, tempY, tempZ);
-		vecXA[i] = vecX; vecYA[i] = vecY; vecZA[i] = vecZ;
+		vecX /= mlrd; vecY /= mlrd; vecZ /= mlrd;
+		printf("Angle: %d / 360. \n Vector X: %d ; Vector Y: %d ; Vector Z: %d ;\nTemp X: %d ; Temp Y: %d ; Temp Z: %d ;\n\n *************************************** \n\n", i, vecX, vecY, vecZ, tempX, tempY, tempZ);
+		vecXA[i][0] = vecX; vecYA[i][0] = vecY; vecZA[i][0] = vecZ;
+		vecXA[i][1] = angle; vecYA[i][1] = angle; vecZA[i][1] = angle;
+		Sleep(2);
 	}
-	int maxX = vecXA[0], maxY = vecYA[0], maxZ = vecZA[0];
-	for (i = 0; i < 72; i++)
+	int maxX = vecXA[0][0], maxY = vecYA[0][0], maxZ = vecZA[0][0];
+	int maxAngleX = 0, maxAngleY = 0, maxAngleZ = 0;
+	for (int i = 0; i < 72; i++)
 	{
-		if (vecXA[i] > maxX)
-			maxX = vecXA[i];
-		if (vecYA[i] > maxY)
-			maxY = vecYA[i];
-		if (vecZA[i] > maxZ)
-			maxZ = vecZA[i];
+		if (vecXA[i][0] > maxX)
+		{
+			maxX = vecXA[i][0];
+			maxAngleX = vecXA[i][1];
+		}
+		if (vecYA[i][0] > maxY)
+		{
+			maxY = vecYA[i][0];
+			maxAngleY = vecYA[i][1];
+		}
+		if (vecZA[i][0] > maxZ)
+		{
+			maxZ = vecZA[i][0];
+			maxAngleZ = vecZA[i][1];
+		}
 	}
 
-	printf("Maximal vectors was: %d %d %d", maxX, maxY, maxZ);
+	printf("Maximal vectors was: \nX: %d ; Y: %d ; Z: %d ;\nAt angles: \nX: %d ; Y: %d ; Z: %d ;\n", maxX, maxY, maxZ, maxAngleX, maxAngleY, maxAngleZ);
+	puts("Trying to orient on sun...");
+	float oXYZ[3] = {maxAngleX, 0.0, 0.0};
+	send_unican_message(0x5, 0xF010, (uint8_t*)(oXYZ), 96);
 }
